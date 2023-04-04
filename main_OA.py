436f9bdf33b03c360246e5d0eced60ef86e43969
carcase2 <carcase2.icloud.com>
2023-04-04 14:26:31 +0900

    add:e-mail 주소 확인하기

--- main_OA.py
+++ main_OA.py
@@ -8,6 +8,13 @@ from PySide6.QtGui import QImage, QPixmap
 # Ensure you have the correct Tesseract executable path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+
+people = {
+    'names': [],
+    'ages': [],
+    'phone': [],
+}
+
 def read_business_card(image_path):
     # Load the image
     image = cv2.imread(image_path)
@@ -17,14 +24,53 @@ def read_business_card(image_path):
        return ""
 
     # Preprocess the image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # 이미지 스케일 업
+    image_upscale = cv2.pyrUp(image)
+    
+    image_upscale2 = cv2.pyrUp(image_upscale)
+    
+    saved = cv2.imwrite('saved_image.png', image_upscale)
+    
+    # image 내용 확인 
+    height,width,channels= image.shape
+    print(height,width,channels)
+    
+    height,width,channels= image_upscale.shape
+    print(height,width,channels)
+    
+    height,width,channels= image_upscale2.shape
+    print(height,width,channels)
+    
+    saved = cv2.imwrite('saved_image2.png', image_upscale2)
+
+    
+    gray = cv2.cvtColor(image_upscale2, cv2.COLOR_BGR2GRAY)
     _, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     # Extract text using OCR
     text_kor = pytesseract.image_to_string(threshold, lang='kor')
     text_eng = pytesseract.image_to_string(threshold)
-
+        
+    text_eng = text_eng.replace("!", "1")
+    
+    # print(text_eng)
+    # print(text_kor)
+    email = extract_email(text_eng)
+    
+    if email:
+        print(f"Email: {email}")
+    else:
+        print("No email found.")
     return text_kor,text_eng
+
+def extract_email(text):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    result = re.search(pattern, text)
+    if result:
+        return result.group()
+    return None
 
 class BusinessCardReader(QWidget):
     def __init__(self):
@@ -66,18 +112,23 @@ class BusinessCardReader(QWidget):
             self.label_eng.setText(text_eng_without_spaces)
         
         pattern = r'\b010[-\s]?\d{3,4}[-\s]?\d{4}\b'
-        result_1 = re.search(pattern, text_kor_without_spaces)
-        result_2 = re.search(pattern, text_eng_without_spaces)        
-        print(result_1.group())
-        print(result_2.group())
+        result_1 = re.search(pattern, text_kor)
+        result_2 = re.search(pattern, text_eng)        
         
-        # text_kor_without_spaces = "".join(text_kor.split())
+        
+        if result_1 is not None and result_2 is not None:
+            print("휴대폰",result_1.group())
+            print("휴대폰",result_2.group())
+        else:            
+            print("No match found.")
+            
+        # Append values for the second person
+        people['names'].append('Jane Smith')
+        people['ages'].append(28)
+        people['phone'].append(result_1.group())
 
-        
-        for character in text_kor_without_spaces:
-            print(character)
-        
-
+        print(people['phone'])
+                
 def main():
     app = QApplication(sys.argv)
     window = BusinessCardReader()
